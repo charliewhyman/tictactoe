ec0ea9548bbe8b9d34ee8db6bf568727b5276739
cwhyman <cwhyma@gmail.com>
2021-08-16 19:44:06 +0100

    Added game win and end functionality

--- js/script.js
+++ js/script.js
@@ -1,19 +1,12 @@
 'use strict';
 
-//create a factory to generate each player
-let XPiece = 'X';
-let OPiece = 'O';
+//define
 let turnValue = 1;
 
-let selectedPiece = XPiece;
-
-//create a test array for the gameBoard
+//create a blank array for the gameBoard
 const gameBoardArray = Array(9).fill('')
-let XArray = Array(9).fill('')
-let OArray = Array(9).fill('')
 
-let selectedArray = XArray;
-
+//define the winning combinations by array positions in a 9x9 grid
 let winArrays = [
     [0,1,2],
     [3,4,5],
@@ -25,15 +18,17 @@ let winArrays = [
     [2,4,6]
 ];
 
-
-const playerFactory = (name, piece) => {
-    return { name, piece };
+//create a factory to generate each player
+const playerFactory = (name, piece, array) => {
+    return { name, piece, array };
   };
   
-const player1 = playerFactory('player1', 'X');
-const player2 = playerFactory('player2', 'X');
+//create two players
+const player1 = playerFactory('Player 1', 'X', Array(9).fill(''));
+const player2 = playerFactory('Player 2', 'O', Array(9).fill(''));
 
-
+//for now, let player 1 (X) have the first turn
+let selectedPlayer = player1;
 
 // create a module for the gameBoard
 const displayController = (() => {
@@ -50,17 +45,15 @@ const displayController = (() => {
     };
         const showPlayer = () => {
         const playerIndicator = document.getElementById('playerIndicator');
-        playerIndicator.textContent = `Current player: ${selectedPiece}`;
+        playerIndicator.textContent = `Current player: ${selectedPlayer.piece}`;
         };
 
         const switchPlayer = () => {
-        if (selectedPiece === 'X') {
-            selectedPiece = 'O';
-            selectedArray = OArray;
+        if (selectedPlayer.name === 'Player 1') {
+            selectedPlayer = player2;
         } else {
-            selectedPiece = 'X'
-            selectedArray = XArray;
-        };
+            selectedPlayer = player1;
+            };
         };
 
         const nextTurn = () => {
@@ -70,57 +63,70 @@ const displayController = (() => {
         showPlayer();
             };
 
+        const endGame = () => {
+            console.log('GAME END')
+            };
+
     return {   
         create,
         switchPlayer,
         showPlayer,
-        nextTurn
+        nextTurn,
+        endGame
     };
 })();
 
 displayController.create();
-displayController.nextTurn();
 
 
 const gameBoard = (() => {
     let squareCountArray = Array(9).fill(0);
+    
+    //create a function to check the player's current array against the win combinations
     const checkWin = () => {
-        for (const winArray of winArrays) { // You can use `let` instead of `const` if you like
+        for (let winArray of winArrays) { 
             let checker = (arr, target) => target.every(v => arr.includes(v));
-            let winStatus = checker(selectedArray, winArray)
+            let winStatus = checker(selectedPlayer.array, winArray)
             if (winStatus === true) {
-                console.log('true')
-                winIndicator.textContent = `${selectedPiece} WINS!`;
-                
-            } else {
-                console.log('false')
-            }
+                winIndicator.textContent = `${selectedPlayer.name} wins in ${turnValue} turns!`;
+                return true;  
+            } 
         };
     };
+
+    //create a function to check the player's current array against the win combinations
+    const checkGameEnd = () => {
+        if (checkWin() || squareCountArray.every(e => e === 1)) {
+            displayController.endGame();
+        } else {
+            displayController.nextTurn();
+            turnValue++;
+        }
+    };
+
+    //create a function to render the contents of the gameBoard array
     const create = () => {
-        //create a function to render the contents of the gameBoard array
         const grid = document.getElementById('gameBoardGrid');
+
+        //add event listeners to each square in the grid
         grid.addEventListener('click', (event) => {
             let clickedElement = event.target;
             let isSquare = clickedElement.className === 'gameBoardSquare';
             
             if (isSquare && squareCountArray[clickedElement.id] === 0) {
-                clickedElement.textContent = selectedPiece;
-                selectedArray[clickedElement.id] = parseInt(clickedElement.id);
-                console.log('id: '+clickedElement.id);
-                console.log(selectedPiece);
-                console.log(selectedArray);
+                clickedElement.textContent = selectedPlayer.piece;
+                selectedPlayer.array[clickedElement.id] = parseInt(clickedElement.id);
                 squareCountArray[clickedElement.id] = 1;
 
+                console.log(squareCountArray)
                 checkWin();
-                displayController.nextTurn();
-                turnValue++;
+                checkGameEnd();
             };
         });
     };
     return {   
         create,
-        checkWin
+        checkWin,
         ///messages
     };
     }
@@ -128,27 +134,3 @@ const gameBoard = (() => {
 )();
     
     gameBoard.create();
-
-
-
-
-
-//logic 
-//wins: 
-    //0,1,2
-    //3,4,5
-    //6,7,8
-    //0,3,6
-    //1,4,7
-    //2,5,8
-    //0,4,8
-    //2,4,6
-    
-
-//a player can only place one piece in their turn
-// a player can only place their piece
-//players cannot place a piece in a used square
-
-// the game finishes when:
-  //pieces have been placed in all nine squares
-  // a player has three of their pieces in a row
